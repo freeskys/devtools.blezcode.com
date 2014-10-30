78fad67873c2f0f1f48928b60a311d6406f0e4f8
Harditya Ramadhan <adit@blezcode.com>
2014-10-30 11:05:12 +0700

    Split to many files to keep it organized

--- js/script.js
+++ js/script.js
@@ -1,19 +1,66 @@
 // Load external template
+function loadExternalTemplate(files, dom) {
+    $.get(files, function(data) {
+        var template = _.template(data);
+        $(dom).html(template);
+    });
+}
+
+// Load Frontend
+    // Load CSS Frameworks
+    loadExternalTemplate('templates/frontend/css/frameworks.html', '#CSSFrameworksContent');
+
+    // Load Javascript
+        // Load Javascript Frameworks
+        loadExternalTemplate('templates/frontend/js/frameworks.html', '#JSFrameworksContent');
+
+        // Load Javascript UI Frameworks
+        loadExternalTemplate('templates/frontend/js/ui_frameworks.html', '#JSUIFrameworksContent');
+
+    // Load Another Front-End Tools
+    loadExternalTemplate('templates/frontend/another.html', '#AnotherFrontendToolsContent');
+
+// Load Backend
+    // Load Java
+        // Load Java Frameworks
+        loadExternalTemplate('templates/backend/java/frameworks.html', '#JavaFrameworksContent');
+
+    // Load Node.js
+        // Load Node.js Micro Frameworks
+        loadExternalTemplate('templates/backend/nodejs/micro_frameworks.html', '#NodejsMicroFrameworksContent');
+
+    // Load PHP
+        // Load PHP CMS
+        loadExternalTemplate('templates/backend/php/cms.html', '#PHPCMSContent');
+
+        // Load PHP Frameworks
+        loadExternalTemplate('templates/backend/php/frameworks.html', '#PHPFrameworksContent');
+
+        // Load PHP Micro Frameworks
+        loadExternalTemplate('templates/backend/php/micro_frameworks.html', '#PHPMicroFrameworksContent');
+
+    // Load Python
+        // Load Python Frameworks
+        loadExternalTemplate('templates/backend/python/frameworks.html', '#PythonFrameworksContent');
+
+        // Load Python Micro Frameworks
+        loadExternalTemplate('templates/backend/python/micro_frameworks.html', '#PythonMicroFrameworksContent');
+
+    // Load Ruby
+        // Load Ruby Frameworks
+        loadExternalTemplate('templates/backend/ruby/frameworks.html', '#RubyFrameworksContent');
+
+        // Load Ruby Micro Frameworks
+        loadExternalTemplate('templates/backend/ruby/micro_frameworks.html', '#RubyMicroFrameworksContent');
+
+    // Load Wordpress
+    loadExternalTemplate('templates/backend/wordpress.html', '#WordPressContent');
 
 // Load iOS
-$.get('templates/ios.html', function(data) {
-    var template = _.template(data);
-    $('#iOSContent').html(template);
-});
+loadExternalTemplate('templates/ios.html', '#iOSContent');
 
 // Load Android
-$.get('templates/android.html', function(data) {
-    var template = _.template(data);
-    $('#AndroidContent').html(template);
-});
+loadExternalTemplate('templates/android.html', '#AndroidContent');
 
 // Load Windows Phone
-$.get('templates/windowsphone.html', function(data) {
-    var template = _.template(data);
-    $('#WPContent').html(template);
-});
+loadExternalTemplate('templates/windows_phone.html', '#WPContent');
